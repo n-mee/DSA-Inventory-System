2fc8c7f3ee12200f0bdf5faa973a4bd6d9d96a9a
N_mee <224951203+n-mee@users.noreply.github.com>
2026-08-18 16:58:40 +0800

    feat: added inventory system functionality - Added display all items - Added sorting items by category - Added item insertion

--- src/controller/controller.c
+++ src/controller/controller.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
 #include "input/input.h"
 #include "cli/display.h"
 #include "service/inventory_service.h"
 #include "controller/controller.h"
 
+static void handle_insertion(InventoryDatabase* inventory) {
+    if (inventory == NULL) return;
+
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+
+    Product new_item = {0};
+
+    if (get_item_credentials(&new_item) == 0) return;
+
+    if (database_add_item(inventory, new_item) == 0) {
+        printf("\nEn error occured during data insertion.\n");
+        return;
+    }
+
+    printf("\nInsertion Completed Successfully\n");
+}
 
 void handle_display_items(InventoryDatabase* inventory, int choice) 
 {
@@ -14,6 +32,10 @@ void handle_display_items(InventoryDatabase* inventory, int choice)
         case 2:
             sort_by_category(inventory);
             view_render_table(inventory);
+            break;
+        case 4:
+            handle_insertion(inventory);
+            break;
         default: break;
     }
 }
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "cli/menus.h"
 #include "input/input.h"
 #include "cli/display.h"
+#include "controller/controller.h"
 #include "model/product_model.h"
 #include "repository/inventory_database.h"
 
@@ -26,6 +27,17 @@ int main() {
         switch (choice) 
         {
             case 1:
+                handle_display_items(&inventory, choice);
+                break;
+            case 2:
+                handle_display_items(&inventory, choice);
+                break;
+            case 3:
+                break;
+            case 4:
+                handle_display_items(&inventory, choice);
+                break;
+            case 5:
                 break;
             case 0:
                 running = false;
