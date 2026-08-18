52baf5345df2183c9e44a5e765b361184700f16f
N_mee <224951203+n-mee@users.noreply.github.com>
2026-08-18 16:53:18 +0800

    feat: added a all in one input catch for inserting data from item insertion

--- include/input/input.h
+++ include/input/input.h
@@ -2,7 +2,9 @@
 #define INPUT_H
 
 #include <stdlib.h>
+#include "model/product_model.h"
 
 int get_input(const char* msg, char* target, size_t size);
+int get_item_credentials(Product* new_item);
 
 #endif
--- src/input/input.c
+++ src/input/input.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "model/product_model.h"
 #include "input/input.h"
 
 int get_input(const char* msg, char* target, size_t size){
@@ -9,5 +10,24 @@ int get_input(const char* msg, char* target, size_t size){
         return 0;
     }
     target[strcspn(target, "\n")] = '\0';
+    return 1;
+}
+
+int get_item_credentials(Product* new_item) {
+    char buffer[100];
+
+    if (get_input("\nEnter product name: ", new_item->itemName, sizeof(new_item->itemName)) == 0 ||
+        get_input("\nEnter product category: ", new_item->category, sizeof(new_item->category)) == 0) {
+        return 0;
+    }
+
+    printf("Enter the product's ID: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, " %d", &new_item->itemId);
+
+    printf("Enter initial price: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer," %lf", &new_item->price);
+
     return 1;
 }
