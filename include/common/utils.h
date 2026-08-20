1e7b8caa640590305c49a89cf46bd4fcf0dc8c1e
N_mee <224951203+n-mee@users.noreply.github.com>
2026-08-20 21:14:54 +0800

    chore: added terminal cli cleaner for UX

--- include/common/utils.h
+++ include/common/utils.h
@@ -1,7 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+    #define sleep_ms(ms) Sleep(ms * 100)
+#elif defined(__linux__)
+    #include <unistd.h>
+    #define sleep_ms(ms) sleep(ms);
+#endif
+
 #include "repository/inventory_database.h"
+
+void clear_screen(unsigned int s);
 
 int find_item_by_id(InventoryDatabase* inventory, int target_id);
 
--- src/common/utils.c
+++ src/common/utils.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "common/utils.h"
+
+
+void clear_screen(unsigned int s) {
+    sleep_ms(s);
+
+    printf("\033[H\033[2J");
+    fflush(stdout);
+}
 
 int find_item_by_id(InventoryDatabase* inventory, int target_id) {
     if (inventory == NULL) return -1;
